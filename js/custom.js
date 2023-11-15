8f9849dc82ae6721f207d4a14ac0f5d6698c447e
Mauricio Miguel <maumiguel.dzovoniarkiewicz@gmail.com>
2023-11-14 22:34:18 -0300

    adicionando spannovo no projeto

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,8 @@
 // instancia jquery e evita conflitos com outras bibliotecas
-jQuery( function($){
+// jQuery( function($){
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel();
 
     let titulos = $('h4') // seletor de tag
 
@@ -8,6 +11,15 @@ jQuery( function($){
     let destaques = $('#featured') // seletor de id
 
     console.log(titulos.first());
+
+    // Configuração de produtos
+
+    $('.featured-item a').addClass('btn btn-dark stretch-link');
+
+    $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>')
+
+
+
 
 
 })
